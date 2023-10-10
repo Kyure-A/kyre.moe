66fe4dfba00fdf6e87040ea34427621961201d3c
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-10-11 00:21:42 +0900

    chore: change route to blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,13 +14,13 @@
   <nav class="px-8 py-4 text-right">
     <a href="/" class="px-2">Accounts</a>
     <a href="/about" class="px-2">About</a>
-    <a href="/blog" class="px-2">Blog</a>
+    <a href="https://kyre.hatenablog.jp" class="px-2">Blog</a>
     <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700">
   </nav>
   
   <div class="flex flex-row">
     <h1 class="text-3xl p-8 font-bold">Kyure_A</h1>
-    <img class="w-32 h-32 rounded-full" src="/kyure_a.jpg" alt="" width="384" height="512">
+    <img class="w-32 h-32 rounded-full" src="/kyure_a.jpg" alt="Kyure_A's icon" width="384" height="512">
   </div>
 </header>
 
