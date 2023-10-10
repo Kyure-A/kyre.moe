8da474c1b1a129c54c856215b8a703d23a53ba51
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-10-11 00:21:21 +0900

    feat: add description

--- src/constants/qualifications.ts
+++ src/constants/qualifications.ts
@@ -1,4 +1,15 @@
-export const qualifications: string[] = [
-  "TOEIC L&R 465",
-  "Paiza プログラミングスキルチェック (S ランク)"
+type Qualification = {
+  name: string,
+  description: string
+}
+
+export const qualifications: Qualification[] = [
+  {
+    name: "TOEIC L&R 465 (L:270, R:195)",
+    description: "高専 1 年生のときに受けたので、今はもう少し上がっていると思います"
+  },
+  {
+    name: "Paiza プログラミングスキルチェック (S ランク)",
+    description: "AtCoder 茶でも取れます"
+  }
 ];
