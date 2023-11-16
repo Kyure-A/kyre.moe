b38cba16238eb5b0b99869b624b690266d52e67e
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-11-16 11:25:20 +0900

    Update experience.ts

--- src/constants/experience.ts
+++ src/constants/experience.ts
@@ -66,7 +66,7 @@ export const experience: Experience[] = [
         team: "✊☺☝️†最強†☝️☺✊",
         result: "",
         result_url: "",
-        description: "",
+        description: "置物として出場しました",
         date: new Date("2023-11-11T00:00:00"),
         public: false,
     },
