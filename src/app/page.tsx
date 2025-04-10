82075ada61d3d06f57cd4a118403b0ecbe3a27c5
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-10 15:23:52 +0900

    chore: resolve path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import Dock, { DockItemData } from "@/blocks/Components/Dock/Dock";
-import Accounts from "@/pages/accounts";
-import Home from "@/pages/home";
+import AboutMe from "@/pages/AboutMe/ui/AboutMe";
+import Accounts from "@/pages/Accounts/ui/Accounts";
+import Home from "@/pages/Home/ui/Home";
+
+import Dock, { DockItemData } from "@/shared/ui/Dock/Dock";
 import { useState } from "react";
 import { FaHome } from "react-icons/fa";
-import { FaUser } from "react-icons/fa6";
+import { FaClock, FaMessage, FaUser } from "react-icons/fa6";
 import { MdOutlineDescription } from "react-icons/md";
 
 export default function Main() {
     const [currentPage, setCurrentPage] = useState("Home");
 
     const items: DockItemData[] = [
-        { icon: <FaHome/>, label: "Home", onClick: () => setCurrentPage("Home")},
         { icon: <FaUser/>, label: "Accounts", onClick: () => setCurrentPage("Accounts")},
-        { icon: <MdOutlineDescription/>, label: "About me", onClick: () => setCurrentPage("About")}
+        { icon: <FaClock />, label: "History", onClick: () => setCurrentPage("Timeline")},
+        { icon: <FaHome/>, label: "Home", onClick: () => setCurrentPage("Home")},
+        { icon: <MdOutlineDescription/>, label: "About me", onClick: () => setCurrentPage("About")},
+        { icon: <FaMessage />, label: "Posts", onClick: () => setCurrentPage("Posts") }
     ]
     
     return (
@@ -22,8 +26,9 @@ export default function Main() {
           <main className="items-center">
             {currentPage === "Home" && <Home />}
             {currentPage === "Accounts" && <Accounts /> }
+            {currentPage === "About" && <AboutMe /> }
           </main>
-          <footer className="items-center fixed bottom-0 left-0 right-0">
+          <footer className="items-center fixed bottom-0 left-0 right-0 z-40">
             <Dock
                 items={items}
                 panelHeight={70}
