2c7202fab55156e887b7f99c0e617703f8a8ff87
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-10 19:47:03 +0900

    chore: routing and add language switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,36 +4,64 @@ import Accounts from "@/pages/Accounts/ui/Accounts";
 import Home from "@/pages/Home/ui/Home";
 
 import Dock, { DockItemData } from "@/shared/ui/Dock/Dock";
-import { useState } from "react";
 import { FaHome } from "react-icons/fa";
-import { FaClock, FaMessage, FaUser } from "react-icons/fa6";
-import { MdOutlineDescription } from "react-icons/md";
+import { FaClock, FaUser } from "react-icons/fa6";
+import { MdDescription } from "react-icons/md";
+import LanguageToggle from "../shared/ui/LanguageToggleSwitch/LanguageToggleSwitch";
+import History from "@/pages/History/History";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { ReactNode } from "react";
 
-export default function Main() {
-    const [currentPage, setCurrentPage] = useState("Home");
+type AppLayoutProps = {
+    children: ReactNode;
+}
 
+const NavigationDock = () => {
+    const navigate = useNavigate();
+    
     const items: DockItemData[] = [
-        { icon: <FaUser/>, label: "Accounts", onClick: () => setCurrentPage("Accounts")},
-        { icon: <FaClock />, label: "History", onClick: () => setCurrentPage("Timeline")},
-        { icon: <FaHome/>, label: "Home", onClick: () => setCurrentPage("Home")},
-        { icon: <MdOutlineDescription/>, label: "About me", onClick: () => setCurrentPage("About")},
-        { icon: <FaMessage />, label: "Posts", onClick: () => setCurrentPage("Posts") }
-    ]
+        { icon: <FaHome/>, label: "Home", onClick: () => navigate("/")},
+        { icon: <FaUser/>, label: "Accounts", onClick: () => navigate("/accounts")},
+        { icon: <FaClock />, label: "History", onClick: () => navigate("/history")},
+        { icon: <MdDescription/>, label: "About", onClick: () => navigate("/about")},
+    ];
     
     return (
+        <footer className="items-center fixed bottom-0 left-0 right-0 z-40">
+          <Dock
+              items={items}
+              panelHeight={70}
+              baseItemSize={50}
+              magnification={60}
+          />
+        </footer>
+    );
+};
+
+const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
+    return (
         <div className="flex flex-col min-h-screen">
-          <main className="items-center">
-            {currentPage === "Home" && <Home />}
-            {currentPage === "Accounts" && <Accounts /> }
+          <header className="fixed items-center top-0 right-0 p-10 z-40">
+            <LanguageToggle onChange={() => {}}/>
+          </header>
+          <main className="flex-1 flex flex-col justify-center items-center">
+            {children}
           </main>
-          <footer className="items-center fixed bottom-0 left-0 right-0 z-40">
-            <Dock
-                items={items}
-                panelHeight={70}
-                baseItemSize={50}
-                magnification={60}
-            />
-          </footer>
+          <NavigationDock />
         </div>
+    );
+};
+
+export default function Main() {
+    return (
+        <Router>
+          <AppLayout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/accounts" element={<Accounts />} />
+              <Route path="/history" element={<History />} />
+            </Routes>
+          </AppLayout>
+        </Router>
     );
 }
