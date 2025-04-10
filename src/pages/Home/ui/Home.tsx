0fca58203b2f6a3fd5f6246ab72e4215f17030fe
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-10 15:16:47 +0900

    chore(home): add background

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -1,34 +1,42 @@
-
 import ASCIIText from "@/shared/ui/ASCIIText/ASCIIText";
+import Balatro from "@/shared/ui/Balatro/Balatro";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-export default function Home () {
+export default function Home() {
     const [isEnglish, setIsEnglish] = useState(true);
-
     useEffect(() => {
         const timer = setInterval(() => {
             setIsEnglish(previousState => !previousState);
         }, 5000)
-
         return () => clearInterval(timer);
     }, [])
     
     return (
-        <div className="flex flex-col">
-          <div className="flex flex-col items-center">
-            <ASCIIText
-                text={ isEnglish ? "Kyure_A" : "キュレェ"}
-                asciiFontSize={12}
-                textFontSize={20}/>
-          </div>
-          <div className="flex flex-col items-center center">
-            <Image
-                alt="Kyure_A"
-                src="/kyre.png"
-                width={1000}
-                height={1000}
+        <div className="relative w-screen h-screen overflow-hidden">
+          <div className="absolute inset-0 w-full h-full z-0">
+            <Balatro
+                pixelFilter={250}
+                color1="#272822"
+                color2="#F92672"
+                color3="#66D9EF"
             />
+          </div>
+          <div className="absolute flex flex-col z-10 pt-16 inset-0 ">
+            <div className="flex flex-col items-center mb-6">
+              <ASCIIText
+                  text={ isEnglish ? "Kyure_A" : "キュレェ"}
+                  asciiFontSize={12}
+                  textFontSize={20}/>
+            </div>
+            <div className="flex flex-col items-center center">
+              <Image
+                  alt="Kyure_A"
+                  src="/kyre.png"
+                  width={1000}
+                  height={1000}
+              />
+            </div>
           </div>
         </div>
     )
