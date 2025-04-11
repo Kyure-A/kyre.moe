2c189c269ebcdbc02bea874626bde435963ebdac
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-11 10:08:53 +0900

    fix: improve timeline layout

--- src/pages/History/Timeline.tsx
+++ src/pages/History/Timeline.tsx
@@ -16,42 +16,21 @@ type TimelineProps = {
     items?: TimelineItem[]
 }
 
-const TimelineItem: React.FC<TimelineItemProps> = ({ date, title, description }) => {
-    return (
-        <div className="flex mb-8 relative z-10">
-          {/* 左側の日付部分 */}
-          <div className="w-24 text-right pr-4 flex items-center font-medium text-white">
-            {date}
-          </div>
-          
-          {/* 中央のドット */}
-          <div className="relative flex flex-col items-center">
-            <div className="absolute top-1.5 w-4 h-4 rounded-full bg-white border-2 border-white -translate-x-1/2 left-1/2 z-20"></div>
-          </div>
-          
-          {/* 右側のコンテンツ部分 */}
-          <div className="pl-4 flex-col">
-            <h3 className="font-bold text-white">{title}</h3>
-            <p className="text-gray-300 mt-1">{description}</p>
-          </div>
-        </div>
-    );
-};
-
-
 export default function Timeline({ items = [] }: TimelineProps) {
     return (
-        <div className="py-4 relative">
-          <div className="absolute left-24 top-0 bottom-0 w-px bg-gray-300 z-0"></div>
-
+        <ol className="relative border-s border-gray-300 dark:border-gray-700 ml-4">                  
           {items && items.map((item, index) => (
-              <TimelineItem
-                  key={index}
-                  date={item.date}
-                  title={item.title}
-                  description={item.description}
-              />
+              <li key={index} className="mb-10 ms-4">
+                {/* dot */}
+                <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border border-white"></div>
+                
+                <time className="mb-1 text-sm font-normal leading-none text-gray-300">{item.date}</time>
+                
+                <h3 className="text-lg font-semibold text-white">{item.title}</h3>
+                
+                <p className="mb-4 text-base font-normal text-gray-300">{item.description}</p>
+              </li>
           ))}
-        </div>
+        </ol>
     );
-};
+}
