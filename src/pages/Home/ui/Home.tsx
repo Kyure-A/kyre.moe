7cb50d24b624f0d56d833542febfb3d0935186c4
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-11 16:57:45 +0900

    feat: glitch image

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import FadeTextRotator from "./FadeTextRotator";
 import { srcPath } from "@/shared/lib/path";
 import MysteriousShader from "@/shared/ui/Mys/Mys";
+import GlitchImage from "../../../shared/ui/GlitchImage/GlitchImage";
 
 export default function Home() {
     return (
@@ -19,16 +20,18 @@ export default function Home() {
             />
           </div>
           <div className="absolute flex flex-col z-10 pt-16 inset-0 ">
-            <div className="flex flex-col mb-6">
+            <div className="flex flex-col mb-6 z-20">
               <FadeTextRotator />
             </div>
             <div className="flex flex-col items-center center">
-              <Image
-                  alt="Kyure_A"
-                  src={srcPath("/kyre.png")}
-                  width={1000}
-                  height={1000}
-              />
+              <GlitchImage interval={1} probability={10} intensity={5}>
+                <Image
+                    alt="Kyure_A"
+                    src={srcPath("/kyre.png")}
+                    width={1000}
+                    height={1000}
+                />
+              </GlitchImage>
             </div>
           </div>
         </div>
