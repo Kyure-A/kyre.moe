a6e90bfb0cbb73a04df22d5ab8676e0ce8e8588a
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-05-09 09:32:51 +0900

    style: format by prettier

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: true,
-    output: "export",
-    images: {
-        unoptimized: true
-    },
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
