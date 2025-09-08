e7af7d33335f5d4d2fe4f9ca89393532eecdae2e
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-09-08 19:15:45 +0900

    chore(ui): remove AnimatedContent component usage from Accounts and Timeline
    The AnimatedContent component has been removed from the Accounts and Timeline components to simplify the structure and reduce unnecessary complexity. This change eliminates the `delay` prop and adjusts the JSX structure accordingly.

--- src/pages/History/ui/Timeline.tsx
+++ src/pages/History/ui/Timeline.tsx
@@ -1,4 +1,3 @@
-import AnimatedContent from "@/shared/ui/AnimatedContent/AnimatedContent";
 import React from "react";
 
 export type TimelineItem = {
@@ -16,28 +15,26 @@ export default function Timeline({ items = [] }: TimelineProps) {
   return (
     <ol className="relative border-s border-gray-300 dark:border-gray-700 ml-4">
       {items &&
-        items.map((item, index) => (
-          <AnimatedContent key={index} delay={index * 100}>
-            <a href={item.url || "/"}>
-              <li className="mb-10 ms-4">
-                {/* dot */}
-                <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border border-white"></div>
+       items.map((item, index) => (
+         <a href={item.url || "/"} key={index}>
+           <li className="mb-10 ms-4">
+             {/* dot */}
+             <div className="absolute w-3 h-3 bg-white rounded-full mt-1.5 -start-1.5 border border-white"></div>
 
-                <time className="mb-1 text-sm font-normal leading-none text-gray-400">
-                  {item.date}
-                </time>
+             <time className="mb-1 text-sm font-normal leading-none text-gray-400">
+               {item.date}
+             </time>
 
-                <h3 className="text-lg font-semibold text-white">
-                  {item.title}
-                </h3>
+             <h3 className="text-lg font-semibold text-white">
+               {item.title}
+             </h3>
 
-                <p className="mb-4 text-sm font-normal text-gray-400">
-                  {item.description}
-                </p>
-              </li>
-            </a>
-          </AnimatedContent>
-        ))}
+             <p className="mb-4 text-sm font-normal text-gray-400">
+               {item.description}
+             </p>
+           </li>
+         </a>
+      ))}
     </ol>
   );
 }
