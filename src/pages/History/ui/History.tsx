246abb8bba1178414831fa19179536848341f561
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-11-19 09:54:26 +0900

    fix(history): update timeline data for accuracy and consistency
    Revised timeline entries to improve accuracy and consistency in event descriptions. Adjusted titles, descriptions, and URLs where necessary to reflect the latest information.

--- src/pages/History/ui/History.tsx
+++ src/pages/History/ui/History.tsx
@@ -4,39 +4,20 @@ export default function History() {
   const timelineData: TimelineItem[] = [
     {
       date: "July 10, 2005",
-      title: "生まれる",
-      description: "納豆の日、2700 g くらいの baby",
+      title: "キュレェ 生誕",
+      description: "納豆の日、約 2700 g",
     },
     {
       date: "April, 2021",
-      title: "大阪公立大学工業高等専門学校 入学",
-      description: "ex-大阪府立大学工業高等専門学校",
+      title: "大阪府立大学工業高等専門学校 入学",
+      description: "現: 大阪公立大学工業高等専門学校",
       url: "https://www.ct.omu.ac.jp",
-    },
-    {
-      date: "September, 2022",
-      title: "パソコン甲子園 2022 プログラミング部門 (予選)",
-      description: "順位表凍結時 63 位",
-      url: "https://web.archive.org/web/20220910070431/https://radon.u-aizu.ac.jp/pckosien/stats/pck2022pre_standings.html#:~:text=63,%E9%AB%98%E7%AD%89%E5%B0%82%E9%96%80%E5%AD%A6%E6%A0%A1",
-    },
-    {
-      date: "April, 2023",
-      title:
-        "大阪公立大学工業高等専門学校 総合工学システム学科 電子情報コース 配属",
-      description: "電子:情報 = 7:3 くらい",
-      url: "https://www.ct.omu.ac.jp/courses/electronics-and-information-course/",
     },
     {
       date: "September, 2023",
       title: "パソコン甲子園 2023 プログラミング部門 (予選)",
-      description: "順位表凍結時 35 位 (大阪 1 位)",
-      url: "http://web.archive.org/web/20230909072222/https://radon.u-aizu.ac.jp/pckosien/stats/pck2023pre_standings.html#:~:text=35,%E9%AB%98%E7%AD%89%E5%B0%82%E9%96%80%E5%AD%A6%E6%A0%A1",
-    },
-    {
-      date: "October, 2023",
-      title: "C-Style Inc. に join",
-      description: "Next.js, Nest.js をやっている",
-      url: "https://c-style.net",
+      description: "大阪 1 位 (全体 35 位)",
+      url: "http://web.archive.org/web/20230909072222/https://radon.u-aizu.ac.jp/pckosien/stats/pck2023pre_standings.html",
     },
     {
       date: "October, 2023",
@@ -45,8 +26,9 @@ export default function History() {
     },
     {
       date: "November, 2023",
-      title: "パソコン甲子園 2023 プログラミング部門 (本選)",
-      description: "何もできなかった",
+      title: "パソコン甲子園 2023 プログラミング部門 (予選)",
+      description: "24 位 / 29 チーム",
+      url: "https://web.archive.org/web/20231111083450/https://radon.u-aizu.ac.jp/pckosien/stats/pck2023final_standings.html",
     },
     {
       date: "September, 2024",
