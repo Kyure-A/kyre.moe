42e3ef15fdfb27d8cf18d77383af1b242b62bae9
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2026-01-15 10:11:45 +0900

    chore(blog): switch fs and path imports to namespace form

--- src/shared/lib/blog.ts
+++ src/shared/lib/blog.ts
@@ -1,5 +1,5 @@
-import fs from "node:fs";
-import path from "node:path";
+import * as fs from "fs";
+import * as path from "path";
 import matter from "gray-matter";
 import hljs from "highlight.js";
 import { createMarkdownExit } from "markdown-exit";
