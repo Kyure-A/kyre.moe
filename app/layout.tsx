cff16498b4f04229c1442eda3f673329764d83fc
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2026-01-18 02:07:59 +0900

    feat(blog): add view transition animations for blog navigation and titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,47 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono } from "next/font/google";
+import { ViewTransitions } from "next-view-transitions";
 import "./globals.css";
 import App from "@/app/main";
 
 const ibmPlexMono = IBM_Plex_Mono({
-	subsets: ["latin"],
-	weight: ["500"],
-	variable: "--font-ibm-plex-mono",
-	display: "swap",
+  subsets: ["latin"],
+  weight: ["500"],
+  variable: "--font-ibm-plex-mono",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://kyre.moe"),
-	title: {
-		default: "Kyure_A / キュレェ",
-		template: "%s | Kyure_A",
-	},
-	description: "Kyure_A's portfolio. ",
-	openGraph: {
-		title: "Kyure_A / キュレェ",
-		description: "Kyure_A's portfolio. ",
-		url: "https://kyre.moe",
-		siteName: "Kyure_A",
-		type: "website",
-	},
-	twitter: {
-		card: "summary_large_image",
-	},
+  metadataBase: new URL("https://kyre.moe"),
+  title: {
+    default: "Kyure_A / キュレェ",
+    template: "%s | Kyure_A",
+  },
+  description: "Kyure_A's portfolio. ",
+  openGraph: {
+    title: "Kyure_A / キュレェ",
+    description: "Kyure_A's portfolio. ",
+    url: "https://kyre.moe",
+    siteName: "Kyure_A",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" className={ibmPlexMono.variable}>
-			<body suppressHydrationWarning={true}>
-				<App>{children}</App>
-			</body>
-		</html>
-	);
+  return (
+    <ViewTransitions>
+      <html lang="en" className={ibmPlexMono.variable}>
+        <body suppressHydrationWarning={true}>
+          <App>{children}</App>
+        </body>
+      </html>
+    </ViewTransitions>
+  );
 }
